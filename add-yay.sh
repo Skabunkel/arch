954c2b2e119202fa2eb06536ff02560297690007
Skabunkel <skabunkel@users.noreply.github.com>
2023-04-01 00:36:36 +0200

    fix: facepalm

--- add-yay.sh
+++ add-yay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://aur.archlinux.org/yay-git.git /opt/yay-git
+sudo git clone https://aur.archlinux.org/yay-git.git /opt/yay-git
 sudo chown $USER:$USER /opt/yay-git
 
 cd /opt/yay-git 
