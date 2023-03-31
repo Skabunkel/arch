8b8b0a27d2f975a33ac7bf32bb16bf7d20f8a6e2
Skabunkel <skabunkel@users.noreply.github.com>
2023-04-01 01:57:56 +0200

    stole some lines

--- install-hyprland.sh
+++ install-hyprland.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-yay -S hyprland
+yay -S hyprland swaybg swayidle swaylock wlroots wl-clipboard \
+ waybar-hyprland-git wofi kitty mako grim slurp wf-recorder light \
+ yad thunar geany mpv mpd mpc ncmpcpp viewnior imagemagick xfce-polkit xorg-xwayland
